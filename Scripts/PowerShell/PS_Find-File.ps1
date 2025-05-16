789c4a7a5ab27dd9af2fd8de3ecf04bff72960a5
mndollison <mark.dollison@outlook.com>
2025-05-16 01:37:55 -0500

    Update PS_Find-File.ps1
    Added checking if file exists and appropriate notification. Retained only needed information: FullName, Name, Length, IsHidden, CreationTime, LastWriteTime.

--- Scripts/PowerShell/PS_Find-File.ps1
+++ Scripts/PowerShell/PS_Find-File.ps1
@@ -1,18 +1,34 @@
-#This script is used to find a file in Windows. It accepts two parameters of filename and filepath. Filename is mandatory while filepath is optional.
-# If you don't specify a filepath, it will default to C:\:
-#Example usage:
-#	Find-File -filename "example.txt" 
-#	Find-File -filename "example.txt" -filepath "C:\\Users"
+# This script is used to find a file in Windows. It accepts two parameters of filename and filepath. Filename is mandatory while filepath is optional.
+# If you don't specify a filepath, it will default to C:\
+# To use in a Meterpreter session, follow the steps below:
+#     1. Run command "load powershell"
+#     2. Run command "powershell_import /media/sf_CyberSecurity/PS_Find-File.ps1" (This will load the script to the Meterpreter session.)
+#     3. Run command "powershell_execute "Find-File -filename 'sample.txt' -filepath 'C:\\Users'"
+#
+# Examples on how to call the function:
+# Find-File -filename "example.txt"
+# Find-File -filename "example.txt" -filepath "C:\\Users"
 
 function Find-File {
     param (
         [Parameter(Mandatory=$true)]
         [string]$filename,
-        
+
         [Parameter(Mandatory=$false)]
         [string]$filepath = "C:\\"
     )
-    
-    ##Get-ChildItem -Path $filepath -Recurse -ErrorAction SilentlyContinue -Include $filename | ForEach-Object { $_.FullName }
-    Get-ChildItem -Path $filepath -Recurse -Force -Include $filename -ErrorAction SilentlyContinue | Format-List -Property *
+
+    # Search for the file 
+    $files = Get-ChildItem -Path $filepath -Recurse -Force -Include $filename -ErrorAction SilentlyContinue
+
+    # Check if any files were found
+    if ($files) {
+        # Select desired properties including CreationTime and LastWriteTime
+        $files | Select-Object -Property FullName, Name, Length, @{
+            Name = "IsHidden"
+            Expression = { ($_.Attributes -band [System.IO.FileAttributes]::Hidden) -ne 0 }
+        }, CreationTime, LastWriteTime | Format-Table -AutoSize
+    } else {
+        Write-Output "File '$filename' not found in '$filepath'."
+    }
 }
